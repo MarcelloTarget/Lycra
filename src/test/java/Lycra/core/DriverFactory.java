a846f75e574dae16a5cceb14fbff7dbbbc2c6cf1
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-17 18:13:05 -0300

    AT

---
+++ src/test/java/Lycra/core/DriverFactory.java
@@ -0,0 +1,35 @@
+package Lycra.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
+
+	private DriverFactory() {
+	}
+
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+
+	public static WebDriver initDriver() {
+		WebDriver driver = null;
+		switch (Propriedades.browser) {
+		
+		case CHROME:
+			//System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		}
+		driver.manage().window().maximize();
+		return driver;
+	}
+}
